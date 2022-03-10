8cce8d6501a0c0553dc8a0e7190d756ed4ed8d62
ErickBss <erickbasso22@gmail.com>
2022-03-10 15:18:31 -0300

    styled_header_home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,70 @@
+import { FiUser } from 'react-icons/fi';
+import { AiOutlineCalendar } from 'react-icons/ai';
+
 import Header from '../components/Header';
 
 import styles from './home.module.scss';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
       <Header />
 
       <main className={styles.container}>
-        <h1>Como utilizar Hooks</h1>
-        <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
-        <div className={styles.infoContent}>
-          <time>15 Mar 2021</time>
-          <p>Erick Basso</p>
-        </div>
+        <section className={styles.articleBlocks}>
+          <h1>Como utilizar Hooks</h1>
+          <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
+          <div className={styles.infoContent}>
+            <div>
+              <i>
+                <AiOutlineCalendar />
+              </i>
+              <time>15 Mar 2021</time>
+            </div>
+
+            <div>
+              <i>
+                <FiUser />
+              </i>
+              <p>Erick Basso</p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.articleBlocks}>
+          <h1>Como utilizar Hooks</h1>
+          <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
+          <div className={styles.infoContent}>
+            <div>
+              <i>
+                <AiOutlineCalendar />
+              </i>
+              <time>15 Mar 2021</time>
+            </div>
+
+            <div>
+              <i>
+                <FiUser />
+              </i>
+              <p>Erick Basso</p>
+            </div>
+          </div>
+        </section>
+        <a>Carregar mais posts</a>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = {};
+  return {
+    props: {},
+  };
+};
