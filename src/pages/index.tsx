5df73a60b9bfc9702dfe3d4f5a5e5153f4a74737
ErickBss <erickbasso22@gmail.com>
2022-03-10 14:09:42 -0300

    home_header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,20 @@
-import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
-import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main className={styles.container}>
+        <h1>Como utilizar Hooks</h1>
+        <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
+        <div className={styles.infoContent}>
+          <time>15 Mar 2021</time>
+          <p>Erick Basso</p>
+        </div>
+      </main>
+    </>
+  );
 }
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
-
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-// export default function Home() {
-//   // TODO
-// }
-
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
