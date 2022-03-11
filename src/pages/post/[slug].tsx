814bdc348b2519bf4a4a3630070845e3de08a90a
ErickBss <erickbasso22@gmail.com>
2022-03-11 15:54:13 -0300

    post_page_structure

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,41 @@
+import { RichText } from 'prismic-dom';
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 
-export default function Post() {
-  return <h1>Hello World</h1>;
+type PostProps = {
+  post: {
+    slug: string;
+    image: string;
+    title: string;
+    date: string;
+    author: string;
+    heading: string;
+    content: string;
+  };
+};
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <img />
+
+      <main>
+        <h1></h1>
+
+        <section>
+          <time></time>
+          <p></p>
+          <time></time>
+        </section>
+
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export const getStaticPaths = () => {
@@ -18,11 +50,20 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const response = await prismic.getByUID('post', String(params.slug), {});
 
-  console.log(response);
+  const post = {
+    slug: response.uid,
+    image: response.data.banner.url,
+    title: response.data.title,
+    date: response.first_publication_date,
+    author: response.data.author,
+    heading: response.data.content[0].heading,
+    content: RichText.asText(response.data.content[0].body),
+  };
+  console.log(post);
 
   return {
     props: {
-      params,
+      post,
     },
   };
 };
